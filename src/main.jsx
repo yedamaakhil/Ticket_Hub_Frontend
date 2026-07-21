ff7d7699f1cb124da4feb57d64aa350065540a21
yedamaakhil <yedamaakhil@gmail.com>
2026-07-21 13:52:30 +0530

    Add Clerk

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import { ClerkProvider } from '@clerk/react'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLIC_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLIC_KEY) {
